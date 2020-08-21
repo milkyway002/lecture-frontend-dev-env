874d82751a01807490ead22fb62758c88c6c32ea
milkyway002 <sea_bom_12@naver.com>
2020-08-21 16:06:23 +0900

    loader 과제 수행

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,24 @@ module.exports = {
   entry: {
     main: './src/app.js'
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'file-loader',
+        options: {
+          publicPath: "./dist/",
+          name: "[name].[ext]?[hash]"
+        }
+      }
+    ]
+  },
+
   output: {
     filename: '[name].js',
     path: path.resolve('./dist'),
